ced4eaf19623265df7e9155e4e29002e4aa670ea
Ganesh Alla <allaganesh03@outlook.com>
2024-05-20 18:33:07 +0530

    middle

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
-  const sessionToken = request.cookies.get('next-auth.session-token');
+  const sessionToken = request.cookies.get('next-auth.session-token' || '__Secure-next-auth.session-token');
 
 if (!sessionToken) {
     return NextResponse.redirect(new URL('/login', request.url));
